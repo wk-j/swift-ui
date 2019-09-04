5e432d08c66e73729dc44c70806100ebc0a170eb
wk-j <somnuk.wk@gmail.com>
2019-09-05 00:37:33 +0700

    Initial commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by wk on 5/9/2562 BE.
+//  Copyright © 2562 wk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
